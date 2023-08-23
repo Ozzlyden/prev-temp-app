37ba832e37f72715ca03fda216fd74aa97c18810
Victor Valente <Valente-victor@hotmail.com>
2023-08-23 17:02:11 -0300

    Inicio projeto Previsao do tempo

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h2> Projeto Previsão tempo</h2>
+      </header>
+    </div>
+  );
+}
